25d538341fb554cfc7e51e0f349b6c2035dd2fbb
Ania Blaziak <anblaziak@gmail.com>
2019-04-25 14:36:50 +0100

    change var name from DB to DATABASE

--- src/model/database/db_build.js
+++ src/model/database/db_build.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const dbConnection = require('./db_connection');
-const sql = fs.readFileSync(`${__dirname/db_build.sql}`).toString();
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
 dbConnection.query(sql, (err, res) => {
     if (err) throw err;
--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS dreams CASCADE;
 
 CREATE TABLE dreams (
     id SERIAL PRIMARY KEY NOT NULL,
-    user VARCHAR(20) NOT NULL,
+    name VARCHAR(20) NOT NULL,
     date DATE NOT NULL,
     description TEXT NOT NULL,
     keyword TEXT NOT NULL 
-)
+);
 
-INSERT INTO dreams (user, date, description, keyword) VALUES ('Christopher', '2008-07-08', '(dream(dream(dream(dream(dream(dream(dream(dream(dream(dream(dream(dream))))))))))))', 'inception'),
-('Martin', '1963-08-28', 'I have a dream.', 'freedom')
+INSERT INTO dreams (name, date, description, keyword) VALUES ('Christopher', '2008-07-08', '(dream(dream(dream(dream(dream(dream(dream(dream(dream(dream(dream(dream))))))))))))', 'inception'),
+('Martin', '1963-08-28', 'I have a dream.', 'freedom');
 
 COMMIT;
